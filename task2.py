2be6f63f24413d86fb7e5f1dc95676bc0f816867
Orange <dunduk201@yandex.ru>
2023-01-15 17:34:33 +0900

    task2 was fixed

---
+++ task2.py
@@ -0,0 +1,29 @@
+def coding(text):
+    count = 1
+    res = ''
+    for i in range(len(text)-1):
+        if text[i] == text[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + text[i]
+            count = 1
+    if count > 1 or (text[len(text)-2] != text[-1]):
+        res = res + str(count) + text[-1]
+    return res
+
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+s = input("Введите текст для кодировки: ")
+print(f"Текст после кодировки: {coding(s)}")
+print(f"Текст после дешифровки: {decoding(coding(s))}")
